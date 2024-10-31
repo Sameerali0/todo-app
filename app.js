0c431a8c36db323b6be3e35169a04b8d65906d9d
syedbarimanjan <syedbarimanjan@gmail.com>
2024-10-31 17:08:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const todoForm = document.getElementById("todoForm");
+const todoInput = document.getElementById("todoInput");
+
+
+todoForm.addEventListener("submit",(e) => {
+  e.preventDefault();
+
+  const todoValue = todoInput.value;
+
+  const todoContainer = document.createElement("div");
+  const updateBtn = document.createElement("button");
+  updateBtn.textContent = "Update";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+
+  const todoH1 = document.createElement("h1");
+  todoH1.textContent = todoValue;
+  todoH1.style.display = "Inline";
+
+
+  updateBtn.addEventListener("click", (e) => {
+    const updateInput = document.createElement("input");
+    updateBtn.type = "text";
+    updateInput.value = todoValue;
+
+    todoContainer.replaceChild(updateInput, todoH1)
+  })
+
+  todoContainer.append(todoH1,updateBtn,deleteBtn);
+
+  document.body.appendChild(todoContainer);
+})
